91730ee29fda30c2877807ed6f6b1e21187c6a38
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-24 12:35:10 +0000

    adds next button still need to add icons relates #22

--- src/elm/Routes/AboutYou.elm
+++ src/elm/Routes/AboutYou.elm
@@ -148,4 +148,8 @@ aboutYou model =
                     ]
                 ]
             ]
+        , a
+            [ href "", class "center tc no-underline fw1" ]
+            [ div [ class "white w-20-l w-40-m w-60 bg-gray fw2 center mv5 pa3 br4 fw1 f5 no-underline open-sans" ] [ text "Next" ]
+            ]
         ]
