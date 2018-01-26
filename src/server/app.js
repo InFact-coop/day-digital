47fe77d295e8542159d2cba2cea8ea42e174477c
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-01-26 14:25:54 +0000

    Try other https

--- src/server/app.js
+++ src/server/app.js
@@ -5,12 +5,23 @@ const bodyParser = require("body-parser");
 const api_router = require("./routers/api_router");
 var multer = require("multer");
 var upload = multer({ dest: "uploads/" });
-var sslRedirect = require("heroku-ssl-redirect");
 
 const app = express();
 
+var https_redirect = function(req, res, next) {
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] != "https") {
+      return res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+};
+
 app.use(express.static(path.join(__dirname, "../../public")));
-app.use(sslRedirect());
+app.use(https_redirect);
 app.use(bodyParser.json());
 app.use(upload.single("recordingData"));
 app.use(express.static("public"));
