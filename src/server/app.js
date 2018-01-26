1f11e2f7ef4f21413990834c4c1fcaa061823b6f
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-01-26 14:05:53 +0000

    force https on heroku

--- src/server/app.js
+++ src/server/app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 const api_router = require("./routers/api_router");
 var multer = require("multer");
 var upload = multer({ dest: "uploads/" });
+var sslRedirect = require("heroku-ssl-redirect");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../../public")));
+app.use(sslRedirect());
 app.use(bodyParser.json());
 app.use(upload.single("recordingData"));
 app.use(express.static("public"));
